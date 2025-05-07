23cfab45dc62b894aada4b92e398f77cc7f85359
sivakumar0906 <saisivakumar0906@gmail.com>
2025-05-07 17:03:33 +0530

    firebase-cred-json-changes

--- end_point_middleware/apps.py
+++ end_point_middleware/apps.py
@@ -1,6 +1,8 @@
-from django.apps import AppConfig
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials
+from django.apps import AppConfig
 
 
 class EndPointMiddlewareConfig(AppConfig):
@@ -8,8 +10,12 @@ class EndPointMiddlewareConfig(AppConfig):
     name = 'end_point_middleware'
 
     def ready(self):
-        cred_path = "FIREBASE_CREDENTIALS_JSON"
         if not firebase_admin._apps:
-            cred = credentials.Certificate(cred_path)
-            firebase_admin.initialize_app(cred)
-            print("firebase started")
+            firebase_json = os.environ.get("FIREBASE_CREDENTIALS_JSON")
+            if firebase_json:
+                firebase_dict = json.loads(firebase_json)
+                cred = credentials.Certificate(firebase_dict)
+                firebase_admin.initialize_app(cred)
+                print("Firebase initialized via environment variable")
+            else:
+                print("Firebase credentials not found in environment variables")
