4b6da304a2f14cdd95d6e8a67797f377412fb756
sivakumar0906 <saisivakumar0906@gmail.com>
2025-07-04 16:18:52 +0530

    rag query name as per date - 2

--- chat_main/rag_consumer.py
+++ chat_main/rag_consumer.py
@@ -15,35 +15,39 @@ class CreatorRagConsumer(AsyncJsonWebsocketConsumer):
     async def receive_json(self, content, **kwargs):
         question = content.get("text", "").strip()
         spec = await classify_creator_query(question)
-        if "classification" in spec:
-            classification = spec["classification"]
-            tool = classification.get("tool")
-            args = classification.get("args", {})
-            template = spec.get("template", spec.get("reply", ""))
-        else:
-            tool = spec.get("tool")
-            args = spec.get("args", {})
-            template = spec.get("template", spec.get("reply", ""))
+        tool = spec["classification"]["tool"]
+        args = spec["classification"]["args"]
+        template = spec["template"]
         channel = self.scope["url_route"]["kwargs"]["channel_name"]
+
         if tool == "get_new_users":
-            data = await get_new_users(channel, args.get("date", args.get("period")), args.get("names", False))
+            data = await get_new_users(channel, args["date"], args["names"])
         elif tool == "get_timed_out_users":
-            data = await get_timed_out_users(channel, args.get("date", args.get("period")), args.get("names", False))
+            data = await get_timed_out_users(channel, args["date"], args["names"])
         elif tool == "get_banned_users":
-            data = await get_banned_users(channel, args.get("date", args.get("period")), args.get("names", False))
+            data = await get_banned_users(channel, args["date"], args["names"])
         else:
             return await self.send_json({"type": "response", "text": template})
+
         if isinstance(data, list):
             users = ", ".join(data)
             count = len(data)
         else:
-            count = data
             users = None
+            count = data
+
         try:
-            reply = template.format(count=count, users=users) if users is not None else template.format(count=count)
+            if users is not None:
+                reply = template.format(count=count, users=users)
+            else:
+                reply = template.format(count=count)
         except Exception:
             try:
-                reply = template.format(count, users) if users is not None else template.format(count)
+                if users is not None:
+                    reply = template.format(count, users)
+                else:
+                    reply = template.format(count)
             except Exception:
                 reply = template
+
         await self.send_json({"type": "response", "text": reply})
--- chat_main/rag_tools.py
+++ chat_main/rag_tools.py
@@ -3,9 +3,15 @@ from channels.db import database_sync_to_async
 from create_channels.models import ChannelInvitation
 from chat_main.models import UserModeration
 
+def _parse_date(date_str: str) -> datetime.date:
+    try:
+        return datetime.date.fromisoformat(date_str)
+    except Exception:
+        return datetime.date.today()
+
 @database_sync_to_async
 def get_new_users(channel_name: str, date: str, names: bool):
-    dt = datetime.date.fromisoformat(date)
+    dt = _parse_date(date)
     qs = ChannelInvitation.objects.filter(
         channel_name=channel_name,
         joined_at__date=dt
@@ -16,7 +22,7 @@ def get_new_users(channel_name: str, date: str, names: bool):
 
 @database_sync_to_async
 def get_timed_out_users(channel_name: str, date: str, names: bool):
-    dt = datetime.date.fromisoformat(date)
+    dt = _parse_date(date)
     qs = UserModeration.objects.filter(
         channel_name=channel_name,
         timed_out_initial_time__date=dt
@@ -27,7 +33,7 @@ def get_timed_out_users(channel_name: str, date: str, names: bool):
 
 @database_sync_to_async
 def get_banned_users(channel_name: str, date: str, names: bool):
-    dt = datetime.date.fromisoformat(date)
+    dt = _parse_date(date)
     qs = UserModeration.objects.filter(
         channel_name=channel_name,
         is_banned=True,
