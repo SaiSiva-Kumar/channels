7270ce4ff4eb699a9131838ba9482eef2267b4b8
sivakumar0906 <saisivakumar0906@gmail.com>
2025-07-06 14:40:06 +0530

    adding name to creator coloumn - 1

--- create_channels/models.py
+++ create_channels/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class CreatorChannelData(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
     channel_id = models.AutoField(primary_key=True)
     creator_id = models.CharField(max_length=255)
     channel_name = models.CharField(max_length=255, unique=True, default='temp_channel')
