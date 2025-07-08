5647f55b15544aea0375c0554590128095c57d8b
sivakumar0906 <saisivakumar0906@gmail.com>
2025-07-08 12:46:42 +0530

    level - 1

--- chat_main/routing.py
+++ chat_main/routing.py
@@ -4,4 +4,5 @@ from .rag_consumer import CreatorRagConsumer
 
 websocket_urlpatterns = [
     re_path(r'ws/channel/$', consumers.ChatConsumer.as_asgi()),
+    # re_path(r'ws/rag/(?P<channel_name>[^/]+)/$', CreatorRagConsumer.as_asgi()),
 ]
